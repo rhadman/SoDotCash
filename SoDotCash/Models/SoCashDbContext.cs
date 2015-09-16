dc7486aa712541231f393ce44e8b137a0d6e7c0f
Andrew Mann <andrew@divvycloud.com>
2015-09-15 23:54:38 -0500

    - Create initial Entity Framework model matching ERD

---
+++ SoDotCash/Models/SoCashDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+
+namespace SoDotCash.Models
+{
+    class SoCashDbContext : DbContext
+    {
+        public SoCashDbContext() :
+            base("name=SoCash")
+        {
+        }
+
+        /// <summary>
+        /// Accessor for FinancialInstitution entities in the database
+        /// </summary>
+        public DbSet<FinancialInstitution> FinancialInstitutions { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+
+}
