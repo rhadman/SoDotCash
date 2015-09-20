1f860098a76bacfee7d8aac282901587c76ef70e
Andrew Mann <andrew@divvycloud.com>
2015-09-19 23:56:48 -0500

    - Bind SelectedItem of accounts tree view and attach logic for importing OFX transactions.  This logic is directly in the View Model currently but should be moved to a service.

--- SoDotCash/ViewModels/AccountsViewModel.cs
+++ SoDotCash/ViewModels/AccountsViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using Microsoft.Win32;
+using OFX;
 using OFX.Protocol;
+using OFX.Types;
 
 namespace SoDotCash.ViewModels
 {
@@ -57,6 +61,29 @@ namespace SoDotCash.ViewModels
         }
 
         /// <summary>
+        /// Bound current account
+        /// </summary>
+        public Models.Account SelectedAccount { get; set; }
+
+        /// <summary>
+        /// Provides the collection of transactions for the currently selected account
+        /// </summary>
+        public IEnumerable<Models.Transaction> Transactions
+        {
+            get
+            {
+                if (SelectedAccount == null)
+                    return null;
+                using (var db = new Models.SoCashDbContext())
+                {
+                    // Retrieve account - we need to get an entity in the current db session
+                    var dbAccount = db.Accounts.First(account => account.accountID == SelectedAccount.accountID);
+                    return dbAccount.transactions.ToList();
+                }
+            }
+        }
+
+        /// <summary>
         /// Binding for the Add Account button
         /// </summary>
         private ICommand _addAccountCommand;
@@ -73,6 +100,83 @@ namespace SoDotCash.ViewModels
             // Transition view
             var locator = new ViewModelLocator();
             locator.Main.ActiveViewSource = AddAccountViewModel.ViewXaml;
+        }
+
+
+        /// <summary>
+        /// Binding for the Import Transactions button
+        /// </summary>
+        private ICommand _importTransactionsCommand;
+        public ICommand ImportTransactionsCommand
+        {
+            get { return _importTransactionsCommand ?? (_importTransactionsCommand = new RelayCommand(ImportTransactions, () => SelectedAccount!=null )); }
+        }
+
+        /// <summary>
+        /// Prompts for an OFX file and imports transactions
+        /// </summary>
+        public void ImportTransactions()
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Bank Statements (*.ofx, *.qfx)|*.ofx;*.qfx";
+            if (fileDialog.ShowDialog() != true)
+            {
+                // Cancel selected, ignore
+                return;
+            }
+
+            // TODO: Move the logic below to a service
+
+
+            // Read and convert
+            using (var ofxFileStream = fileDialog.OpenFile())
+            {
+                // Deserialize the OFX file data to an object form
+                var converter = new OFX1ToOFX2Converter(ofxFileStream);
+                foreach (var statement in Statement.CreateFromOFXResponse(converter.ConvertToOFX()))
+                {
+                    using (var db = new Models.SoCashDbContext())
+                    {
+
+                        // Retrieve account - we need to get an entity in the current db session
+                        var updateAccount = db.Accounts.First(account => account.accountID == SelectedAccount.accountID);
+
+                        foreach (var transaction in statement.Transactions)
+                        {
+                            // See if transaction is already in db
+                            try
+                            {
+                                var existingTransaction =
+                                    updateAccount.transactions.First(t => t.fiTransactionId == transaction.TransactionId);
+
+                                // Ensure amount and date of transaction match
+                                existingTransaction.amount = transaction.Amount;
+                                existingTransaction.date = transaction.PostDate.Date;
+                            }
+                            catch (InvalidOperationException)
+                            {
+                                // No such transaction, add entity
+
+                                // Create model transaction
+                                var dbTransaction = new Models.Transaction
+                                {
+                                    amount = transaction.Amount,
+                                    category = "",
+                                    currency = statement.Currency,
+                                    date = transaction.PostDate.Date,
+                                    description = transaction.Name,
+                                    fiTransactionId = transaction.TransactionId
+                                };
+                                updateAccount.transactions.Add(dbTransaction);
+                            }
+                        }
+
+                        //db.Accounts.Add(newAccount);
+                        db.SaveChanges();
+                    }
+                }
+            }
+
         }
 
     }
