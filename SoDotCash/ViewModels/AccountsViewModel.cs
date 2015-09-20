32a1269be37c3d74d033bf7df8c0627d876ecac1
Andrew Mann <andrew@divvycloud.com>
2015-09-19 22:38:22 -0500

    - Add sub-views and associated view models for Accounts view and Add New Account view

---
+++ SoDotCash/ViewModels/AccountsViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using OFX.Protocol;
+
+namespace SoDotCash.ViewModels
+{
+    /// <summary>
+    /// ViewModel handling data exposure and backing functionality for the AccountsView view element
+    /// </summary>
+    public class AccountsViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Name of our view xaml - used for transitions
+        /// </summary>
+        public static readonly string ViewXaml = "AccountsView.xaml";
+
+        /// <summary>
+        /// Provides the collection of accounts mapped by the account type of each
+        /// </summary>
+        public Dictionary<string, ObservableCollection<Models.Account>> AccountsView
+        {
+            get
+            {
+                var accountsByType = new Dictionary<string, ObservableCollection<Models.Account>>();
+
+                // Retrieve the accounts from the database
+                using (var db = new Models.SoCashDbContext())
+                {
+                    // Map all accounts by type
+                    foreach (var account in db.Accounts)
+                    {
+                        // Add category if needed
+                        ObservableCollection<Models.Account> accountList;
+                        if (!accountsByType.ContainsKey(account.accountType))
+                        {
+                            accountList = new ObservableCollection<Models.Account>();
+                            accountsByType.Add(account.accountType, accountList);
+                        }
+                        else
+                        {
+                            accountList = accountsByType[account.accountType];
+                        }
+
+                        // Add to the list under this type
+                        accountList.Add(account);
+                    }
+                }
+                return accountsByType;
+            }
+        }
+
+        /// <summary>
+        /// Binding for the Add Account button
+        /// </summary>
+        private ICommand _addAccountCommand;
+        public ICommand AddAccountCommand
+        {
+            get { return _addAccountCommand ?? (_addAccountCommand = new RelayCommand(AddAccount, () => true)); }
+        }
+
+        /// <summary>
+        /// Transitions to Add Account view
+        /// </summary>
+        public void AddAccount()
+        {
+            // Transition view
+            var locator = new ViewModelLocator();
+            locator.Main.ActiveViewSource = AddAccountViewModel.ViewXaml;
+        }
+
+    }
+}
---
+++ SoDotCash/ViewModels/AddAccountViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using SoDotCash.Models;
+
+namespace SoDotCash.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class AddAccountViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Name of our view xaml - used for transitions
+        /// </summary>
+        public static readonly string ViewXaml = "AddAccountView.xaml";
+
+        /// <summary>
+        /// Initializes a new instance of the AddAccountViewModel class.
+        /// </summary>
+        public AddAccountViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Retrieve all account types for populating a selection list
+        /// </summary>
+        public IEnumerable<AccountType> AllAccountTypes
+        {
+            get { return AccountType.All(); }
+        }
+
+        /// <summary>
+        /// The selected account type
+        /// </summary>
+        public string SelectedAccountType { get; set; }
+
+        /// <summary>
+        /// User entered account name
+        /// </summary>
+        public string AccountName { get; set; }
+
+
+        /// <summary>
+        /// Binding for the Cancel button
+        /// </summary>
+        private ICommand _cancelCommand;
+        public ICommand CancelCommand
+        {
+            get { return _cancelCommand ?? (_cancelCommand = new RelayCommand(Cancel, () => true)); }
+        }
+
+        /// <summary>
+        /// Cancels action. Returns to previous view.
+        /// </summary>
+        public void Cancel()
+        {
+            // Transition to Accounts View
+            var locator = new ViewModelLocator();
+            locator.Main.ActiveViewSource = AccountsViewModel.ViewXaml;
+        }
+
+        /// <summary>
+        /// Binding for the Create Account button
+        /// </summary>
+        private ICommand _createAccountCommand;
+        public ICommand CreateAccountCommand
+        {
+            get { return _createAccountCommand ?? (_createAccountCommand = new RelayCommand(CreateAccount, () => true )); }
+        }
+
+        /// <summary>
+        /// Creates the new account from the information in the form.
+        /// </summary>
+        public void CreateAccount()
+        {
+            // Add the new account
+            using (var db = new Models.SoCashDbContext())
+            {
+                var newAccount = new Account
+                {
+                    accountName = this.AccountName,
+                    accountID = 0,
+                    accountType = SelectedAccountType,
+                    currency = "USD"
+                };
+                db.Accounts.Add(newAccount);
+                db.SaveChanges();
+            }
+
+            // Transition back to accounts view
+            var locator = new ViewModelLocator();
+            locator.Main.ActiveViewSource = AccountsViewModel.ViewXaml;
+        }
+
+
+    }
+}
