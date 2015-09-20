875543d94844df4de25fe28055970ed12465c9c1
Andrew Mann <andrew@divvycloud.com>
2015-09-19 22:39:55 -0500

    - Add top level DB collections for FI Users and Accounts

--- SoDotCash/Models/SoCashDbContext.cs
+++ SoDotCash/Models/SoCashDbContext.cs
@@ -2,6 +2,10 @@
 
 namespace SoDotCash.Models
 {
+    /// <summary>
+    /// The DbContest provides transparent transaction wrappers around access to the database
+    ///  using EntityFramework bound classes representing tables in the database.
+    /// </summary>
     class SoCashDbContext : DbContext
     {
         public SoCashDbContext() :
@@ -11,15 +15,25 @@ namespace SoDotCash.Models
 
         /// <summary>
         /// Accessor for FinancialInstitution entities in the database
+        /// These represent the financial institutions we know about, including access information for OFX-based
+        ///  data retrieval
         /// </summary>
         public DbSet<FinancialInstitution> FinancialInstitutions { get; set; }
 
+        /// <summary>
+        /// Accessor for FinancialInstitutionUser entities in the database.
+        /// These store the user access credentials for the financial institutions.
+        /// </summary>
+        public DbSet<FinancialInstitutionUser> FinancialInstitutionUsers { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
 
-        }
+        /// <summary>
+        /// Accessor for Account entities in the database.
+        /// These store individual accounts which record transactions
+        /// </summary>
+        public DbSet<Account> Accounts { get; set; }
+
+
     }
 
 }
