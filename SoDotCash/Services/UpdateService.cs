e63c18166c526a4fefaaa026fe2f95fd082d2605
Andrew Mann <andrew@divvycloud.com>
2015-09-23 20:35:41 -0500

    - Add UpdateService call to download transactions for an online-access enabled account

--- SoDotCash/Services/UpdateService.cs
+++ SoDotCash/Services/UpdateService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using OFX;
+using SoDotCash.Models;
 
 namespace SoDotCash.Services
 {
@@ -95,6 +97,83 @@ namespace SoDotCash.Services
                 // Pass to stream handler
                 MergeOfxFileIntoAccount(account, ofxFileStream);
             }
+        }
+
+        /// <summary>
+        /// Download OFX transactions for an account and merge them into the account transaction list
+        /// </summary>
+        /// <param name="account">Account configured with financial institution login information</param>
+        public static async void DownloadOfxTransactionsForAccount(Models.Account account)
+        {
+            // Default retrieval parameters
+            OFX.OFX2Service ofxService;
+            OFX.Types.Account ofxAccount;
+            var endTime = DateTimeOffset.Now;
+            var startTime = new DateTimeOffset(new DateTime(1997,1,1));
+
+            using (var db = new Models.SoCashDbContext())
+            {
+                // Retrieve matching account from DB - we need to get an entity in the current db session
+                var updateAccount = db.Accounts.First(dbAccount => dbAccount.accountID == account.accountID);
+
+                // Form FI connection properties for transaction retrieval
+                var fi = new OFX.Types.FinancialInstitution(
+                    updateAccount.financialInstitutionUser.financialInstitution.name,
+                    new Uri(updateAccount.financialInstitutionUser.financialInstitution.ofxUpdateUrl),
+                    updateAccount.financialInstitutionUser.financialInstitution.ofxOrganizationId,
+                    updateAccount.financialInstitutionUser.financialInstitution.ofxFinancialUnitId
+                    );
+
+                // Form credentials for login
+                var credentials = new OFX.Types.Credentials(
+                    updateAccount.financialInstitutionUser.userId,
+                    updateAccount.financialInstitutionUser.password
+                    );
+
+                // Create service
+                ofxService = new OFX2Service(fi, credentials);
+
+                // Create proper account type for this account
+                var accountType = (Models.AccountType) account.accountType;
+                if (accountType == AccountType.CHECKING)
+                {
+                    // Split routing and account id from combined string
+                    var accountIdComponents = account.fiAccountID.Split(':');
+                    ofxAccount = new OFX.Types.CheckingAccount(accountIdComponents[0], accountIdComponents[1], "", true);
+                }
+                else if (accountType == AccountType.SAVINGS)
+                {
+                    // Split routing and account id from combined string
+                    var accountIdComponents = account.fiAccountID.Split(':');
+                    ofxAccount = new OFX.Types.SavingsAccount(accountIdComponents[0], accountIdComponents[1], "", true);
+                }
+                else //if (accountType == AccountType.CREDITCARD)
+                {
+                    ofxAccount = new OFX.Types.CreditCardAccount(account.fiAccountID, "", true);
+                }
+
+                // Use the start time of the latest transaction if we have any
+                try
+                {
+                    var lastTransaction =
+                        (from transaction in updateAccount.transactions
+                         orderby transaction.date descending
+                         select transaction).First();
+                    startTime = new DateTimeOffset(lastTransaction.date);
+
+                }
+                catch (InvalidOperationException)
+                {
+                    // No transactions - ignore and use default start date.
+                }
+
+            }
+
+            // Retrieve statement(s) (should only be one per protocol, but we can handle any number)
+            var ofxStatments = await ofxService.GetStatement(ofxAccount, startTime, endTime);
+            foreach (var ofxStatement in ofxStatments)
+                    MergeStatementTransactionsIntoAccount(account, ofxStatement);
+
         }
 
     }
