81272e57b834944962671b2974d5ab126729fc90
Andrew Mann <andrew@divvycloud.com>
2015-09-25 22:37:11 -0500

    - Add DataServices class to contain service functionality that provides access to application data

---
+++ SoDotCash/Services/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoDotCash.Models;
+
+namespace SoDotCash.Services
+{
+    /// <summary>
+    /// Provides functionality for accessing and manipulating application data.
+    /// Reusable application logic should be placed here
+    /// </summary>
+    public class DataService
+    {
+        /// <summary>
+        /// Determine if there are any existing accounts in the database
+        /// </summary>
+        /// <returns>
+        /// True - There are existing accounts
+        /// False - There are no accounts
+        /// </returns>
+        public static bool AnyExistAccounts()
+        {
+            using (var db = new SoCashDbContext())
+            {
+                return db.Accounts.Any();
+            }
+        }
+    }
+}
