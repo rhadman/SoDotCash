f2e7d0a0c95a4cd2222d8b3b18d5c90f3c855f70
James Thomas <progThomas.jctsoft@gmail.com>
2015-09-29 20:15:09 -0400

    added converted to assist in ledger stylings

---
+++ SoDotCash/Converters/NumberToIsPositiveConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SoDotCash.Converters
+{
+    [ValueConversion(typeof(int), typeof(bool))]
+    class NumberToIsPositiveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (decimal)value > 0 ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
