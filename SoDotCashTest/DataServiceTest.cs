ce184daf7044a6130b0770e709de52bb088d7d6f
Andrew Mann <andrew@divvycloud.com>
2015-10-03 17:58:55 -0500

    - Test: DeleteAccount - Reorganize tests to match order of methods in DataService

--- SoDotCashTest/DataServiceTest.cs
+++ SoDotCashTest/DataServiceTest.cs
@@ -16,86 +16,6 @@ namespace SoDotCashTest
     [TestClass]
     public class DataServiceTest
     {
-        /// <summary>
-        /// Test adding a manual update account
-        /// </summary>
-        [TestMethod]
-        public void TestAddManualUpdateAccount()
-        {
-            // Mock setup for DataService
-            var mockSet = new Mock<DbSet<Account>>();
-            var mockContext = new Mock<SoCashDbContext>();
-            mockContext.Setup(m => m.Accounts).Returns(mockSet.Object);
-
-
-            // Account to add
-            var newAccount = new Account
-            {
-                AccountName = "Test Account",
-                AccountType = AccountType.Checking.ToString(),
-                Currency = "USD"
-            };
-
-            // Add the account in a transaction
-            using (var service = new DataService(mockContext.Object))
-            {
-
-                service.AddAccount(newAccount);
-            }
-
-            // Verify that the service added the account on the mock db exactly once
-            mockSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
-
-            // Verify that the transaction ended properly
-            mockContext.Verify(m => m.SaveChanges(), Times.Once());
-        }
-
-        /// <summary>
-        /// Test adding an account with financial institution information
-        /// </summary>
-        [TestMethod]
-        public void TestAddAccountFromFi()
-        {
-            // Mock setup for DataService
-            var mockAccountSet = new Mock<DbSet<Account>>();
-            var mockFiSet = new Mock<DbSet<FinancialInstitution>>();
-            var mockFiUserSet = new Mock<DbSet<FinancialInstitutionUser>>();
-            var mockContext = new Mock<SoCashDbContext>();
-            mockContext.Setup(m => m.Accounts).Returns(mockAccountSet.Object);
-            mockContext.Setup(m => m.FinancialInstitutions).Returns(mockFiSet.Object);
-            mockContext.Setup(m => m.FinancialInstitutionUsers).Returns(mockFiUserSet.Object);
-
-
-            // Account to add
-            var newAccount = new Account
-            {
-                AccountName = "Test Account",
-                AccountType = AccountType.Checking.ToString(),
-                Currency = "USD"
-            };
-
-            // Dummy FI
-            var financialInstitution = new OFX.Types.FinancialInstitution("Test FI", new Uri("http://test.com/"),"Test Org ID", "Test Unit ID");
-
-            // Dummy FI User
-            var financialInstitutionUser = new FinancialInstitutionUser { UserId = "Test User", Password = "Test Password"};
-
-
-            // Add the account in a transaction
-            using (var service = new DataService(mockContext.Object))
-            {
-
-                service.AddAccount(newAccount, financialInstitution, financialInstitutionUser);
-            }
-
-            // Verify that the service added 1 account, 1 fi and 1 fi user
-            mockAccountSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
-            mockFiSet.Verify(m => m.Add(It.IsAny<FinancialInstitution>()), Times.Once());
-            mockFiUserSet.Verify(m => m.Add(It.IsAny<FinancialInstitutionUser>()), Times.Once());
-
-            // Verify that the transaction ended properly
-            mockContext.Verify(m => m.SaveChanges(), Times.Once());
-        }
 
         /// <summary>
         /// Test functionality for determining if any accounts exist
@@ -185,7 +105,7 @@ namespace SoDotCashTest
         }
 
         /// <summary>
-        /// Test adding a transaction
+        /// Test deleting a transaction
         /// </summary>
         [TestMethod]
         public void TestDeleteTransaction()
@@ -235,10 +155,6 @@ namespace SoDotCashTest
                 FiTransactionId = "TRN1"
             };
 
-            // Mock for Entity() call on context
-            var mockEntityEntry = new Mock<DbEntityEntry<Transaction>>();
-            mockEntityEntry.SetupAllProperties();
-
             // Mock setup for DataService
             var mockTransactionSet = new Mock<DbSet<Transaction>>();
             var mockContext = new Mock<SoCashDbContext>();
@@ -255,5 +171,117 @@ namespace SoDotCashTest
             // Verify that the transaction ended properly
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
+
+        /// <summary>
+        /// Test adding a manual update account
+        /// </summary>
+        [TestMethod]
+        public void TestAddManualUpdateAccount()
+        {
+            // Mock setup for DataService
+            var mockSet = new Mock<DbSet<Account>>();
+            var mockContext = new Mock<SoCashDbContext>();
+            mockContext.Setup(m => m.Accounts).Returns(mockSet.Object);
+
+
+            // Account to add
+            var newAccount = new Account
+            {
+                AccountName = "Test Account",
+                AccountType = AccountType.Checking.ToString(),
+                Currency = "USD"
+            };
+
+            // Add the account in a transaction
+            using (var service = new DataService(mockContext.Object))
+            {
+
+                service.AddAccount(newAccount);
+            }
+
+            // Verify that the service added the account on the mock db exactly once
+            mockSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
+
+            // Verify that the transaction ended properly
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        /// <summary>
+        /// Test adding an account with financial institution information
+        /// </summary>
+        [TestMethod]
+        public void TestAddAccountFromFi()
+        {
+            // Mock setup for DataService
+            var mockAccountSet = new Mock<DbSet<Account>>();
+            var mockFiSet = new Mock<DbSet<FinancialInstitution>>();
+            var mockFiUserSet = new Mock<DbSet<FinancialInstitutionUser>>();
+            var mockContext = new Mock<SoCashDbContext>();
+            mockContext.Setup(m => m.Accounts).Returns(mockAccountSet.Object);
+            mockContext.Setup(m => m.FinancialInstitutions).Returns(mockFiSet.Object);
+            mockContext.Setup(m => m.FinancialInstitutionUsers).Returns(mockFiUserSet.Object);
+
+
+            // Account to add
+            var newAccount = new Account
+            {
+                AccountName = "Test Account",
+                AccountType = AccountType.Checking.ToString(),
+                Currency = "USD"
+            };
+
+            // Dummy FI
+            var financialInstitution = new OFX.Types.FinancialInstitution("Test FI", new Uri("http://test.com/"), "Test Org ID", "Test Unit ID");
+
+            // Dummy FI User
+            var financialInstitutionUser = new FinancialInstitutionUser { UserId = "Test User", Password = "Test Password" };
+
+
+            // Add the account in a transaction
+            using (var service = new DataService(mockContext.Object))
+            {
+
+                service.AddAccount(newAccount, financialInstitution, financialInstitutionUser);
+            }
+
+            // Verify that the service added 1 account, 1 fi and 1 fi user
+            mockAccountSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
+            mockFiSet.Verify(m => m.Add(It.IsAny<FinancialInstitution>()), Times.Once());
+            mockFiUserSet.Verify(m => m.Add(It.IsAny<FinancialInstitutionUser>()), Times.Once());
+
+            // Verify that the transaction ended properly
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        /// <summary>
+        /// Test deleting and account
+        /// </summary>
+        [TestMethod]
+        public void TestDeleteAccount()
+        {
+            // Account for test
+            var account = new Account
+            {
+                AccountName = "Test Account",
+                AccountType = AccountType.Checking.ToString(),
+                Currency = "USD"
+            };
+
+            // Mock setup for DataService
+            var mockAccountSet = new Mock<DbSet<Account>>();
+            var mockContext = new Mock<SoCashDbContext>();
+            mockContext.Setup(m => m.Set<Account>()).Returns(mockAccountSet.Object);
+
+            // Delete the account
+            using (var service = new DataService(mockContext.Object))
+                service.DeleteAccount(account);
+
+            // Verify that the service removed the account on the mock db exactly once
+            mockAccountSet.Verify(m => m.Remove(It.IsAny<Account>()), Times.Once());
+
+            // Verify that the transaction ended properly
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
     }
 }
