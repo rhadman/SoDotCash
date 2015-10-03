8c0f596eaccd2292313cad16e6f3aa6b30fafa82
Andrew Mann <andrew@divvycloud.com>
2015-10-03 00:17:29 -0500

    - Re-add account configuration

---
+++ SoDotCash/Views/AccountsViewTabs/AccountConfiguration.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+
+namespace SoDotCash.Views.AccountsViewTabs
+{
+    /// <summary>
+    /// Interaction logic for AccountConfiguration.xaml
+    /// </summary>
+    public partial class AccountConfiguration
+    {
+        public AccountConfiguration()
+        {
+            InitializeComponent();
+        }
+    }
+}
