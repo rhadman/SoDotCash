ac9bc47f9d089b5d5322929b02a73bf6974f5c04
Andrew Mann <andrew@divvycloud.com>
2015-10-03 14:46:36 -0500

    - Test: Add automatic update account

--- SoDotCashTest/DataServiceTest.cs
+++ SoDotCashTest/DataServiceTest.cs
@@ -16,10 +16,10 @@ namespace SoDotCashTest
     public class DataServiceTest
     {
         /// <summary>
-        /// Test adding an account by copy
+        /// Test adding a manual update account
         /// </summary>
         [TestMethod]
-        public void TestAddAccountCopy()
+        public void TestAddManualUpdateAccount()
         {
             // Mock setup for DataService
             var mockSet = new Mock<DbSet<Account>>();
@@ -44,6 +44,53 @@ namespace SoDotCashTest
 
             // Verify that the service added the account on the mock db exactly once
             mockSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
+
+            // Verify that the transaction ended properly
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        /// <summary>
+        /// Test adding an account with financial institution information
+        /// </summary>
+        [TestMethod]
+        public void TestAddAccountFromFi()
+        {
+            // Mock setup for DataService
+            var mockAccountSet = new Mock<DbSet<Account>>();
+            var mockFiSet = new Mock<DbSet<FinancialInstitution>>();
+            var mockFiUserSet = new Mock<DbSet<FinancialInstitutionUser>>();
+            var mockContext = new Mock<SoCashDbContext>();
+            mockContext.Setup(m => m.Accounts).Returns(mockAccountSet.Object);
+            mockContext.Setup(m => m.FinancialInstitutions).Returns(mockFiSet.Object);
+            mockContext.Setup(m => m.FinancialInstitutionUsers).Returns(mockFiUserSet.Object);
+
+
+            // Account to add
+            var newAccount = new Account
+            {
+                AccountName = "Test Account",
+                AccountType = AccountType.Checking.ToString(),
+                Currency = "USD"
+            };
+
+            // Dummy FI
+            var financialInstitution = new OFX.Types.FinancialInstitution("Test FI", new Uri("http://test.com/"),"Test Org ID", "Test Unit ID");
+
+            // Dummy FI User
+            var financialInstitutionUser = new FinancialInstitutionUser { UserId = "Test User", Password = "Test Password"};
+
+
+            // Add the account in a transaction
+            using (var service = new DataService(mockContext.Object))
+            {
+
+                service.AddAccount(newAccount, financialInstitution, financialInstitutionUser);
+            }
+
+            // Verify that the service added 1 account, 1 fi and 1 fi user
+            mockAccountSet.Verify(m => m.Add(It.IsAny<Account>()), Times.Once());
+            mockFiSet.Verify(m => m.Add(It.IsAny<FinancialInstitution>()), Times.Once());
+            mockFiUserSet.Verify(m => m.Add(It.IsAny<FinancialInstitutionUser>()), Times.Once());
 
             // Verify that the transaction ended properly
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
