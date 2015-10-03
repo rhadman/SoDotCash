3ec88ff41ed9497fd7eb18cd09a8ce7fba75867b
James Thomas <progThomas.jctsoft@gmail.com>
2015-10-03 12:29:12 -0400

    Set border for ledger

--- SoDotCash/Views/AccountsViewTabs/AccountLedger.xaml.cs
+++ SoDotCash/Views/AccountsViewTabs/AccountLedger.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FirstFloor.ModernUI.Presentation;
 
 namespace SoDotCash.Views.AccountsViewTabs
 {
     /// <summary>
     /// Interaction logic for AccountLedger.xaml
     /// </summary>
-    public partial class AccountLedger : UserControl
+    public partial class AccountLedger
     {
+
+        private Color _selectedAccentColor;
+        private readonly LinkCollection _themes = new LinkCollection();
+        private Link _selectedTheme;
+        private string _selectedFontSize;
+
         public AccountLedger()
         {
             InitializeComponent();
+            DatagridTransactions.BorderThickness = new Thickness(1);
+            DatagridTransactions.BorderBrush = new SolidColorBrush(AppearanceManager.Current.AccentColor);
+            
+            AppearanceManager.Current.PropertyChanged += CurrentOnPropertyChanged;
+
+        }
+
+        private void CurrentOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            DatagridTransactions.BorderBrush = new SolidColorBrush(AppearanceManager.Current.AccentColor);
         }
     }
 }
