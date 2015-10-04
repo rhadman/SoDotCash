c9862e01a6e1b3a0f14a5673f910a9b80f1bcbfc
James Thomas <progThomas.jctsoft@gmail.com>
2015-10-04 13:04:28 -0400

    Corrected bug where the accent color would always be set to light blue

--- SoDotCash/ViewModels/MainViewModel.cs
+++ SoDotCash/ViewModels/MainViewModel.cs
@@ -81,19 +81,16 @@ namespace SoDotCash.ViewModels
             //if any of it fails then set it to the default color
             try
             {
-                var convertFromString = ColorConverter.ConvertFromString(Properties.Settings.Default.Accent);
-                if (convertFromString != null)
-                    AppearanceManager.Current.AccentColor = (Color)convertFromString;
-                convertFromString = ColorConverter.ConvertFromString("#FF1BA1E2");
-                if (convertFromString != null)
-                    AppearanceManager.Current.AccentColor = (Color)convertFromString;
+                
+                var colorFromString = ColorConverter.ConvertFromString(Properties.Settings.Default.Accent) ??
+                                      ColorConverter.ConvertFromString("#FF1BA1E2");
+
+                AppearanceManager.Current.AccentColor = (Color)colorFromString;
             }
 
             catch (Exception)
             {
-                var convertFromString = ColorConverter.ConvertFromString("#FF1BA1E2");
-                if (convertFromString != null)
-                    AppearanceManager.Current.AccentColor = (Color)convertFromString;
+                AppearanceManager.Current.AccentColor = (Color)ColorConverter.ConvertFromString("#FF1BA1E2");
             }
         }
 
