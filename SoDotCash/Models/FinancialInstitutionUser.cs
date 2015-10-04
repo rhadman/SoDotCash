f41277c97ceb246ee680ef6e00bf12a3bd81ed01
Andrew Mann <andrew@divvycloud.com>
2015-10-03 20:25:07 -0500

    - Comment Model classes

--- SoDotCash/Models/FinancialInstitutionUser.cs
+++ SoDotCash/Models/FinancialInstitutionUser.cs
@@ -12,21 +12,41 @@ namespace SoDotCash.Models
     [Table("FIUser")]
     public class FinancialInstitutionUser
     {
+        /// <summary>
+        /// So.Cash unique id for this FI User
+        /// </summary>
         [Key]
         public int FiUserId { get; set; }
 
+        /// <summary>
+        /// Identifier of the related financial institution.
+        /// Do not use this field directly. Use FinancialInstitution relationship link.
+        /// </summary>
         public int FiId { get; set; }
 
+        /// <summary>
+        /// User provided login for the OFX online update service
+        /// </summary>
         [StringLength(50)]
         public string UserId { get; set; }
 
+        /// <summary>
+        /// User provided password for the OFX online update service
+        /// </summary>
         [StringLength(50)]
         public string Password { get; set; }
 
+
+        /// <summary>
+        /// Relationship link to the FI this user and credentials apply
+        /// </summary>
         [InverseProperty("Users")]
         [ForeignKey("FiId")]
         public virtual FinancialInstitution FinancialInstitution { get; set; }
 
+        /// <summary>
+        /// Relationship link to So.Cash accounts that are linked to this FI with these credentials
+        /// </summary>
         private ICollection<Account> _accounts;
         public virtual ICollection<Account> Accounts
         {
--- SoDotCash/Models/Transaction.cs
+++ SoDotCash/Models/Transaction.cs
@@ -5,6 +5,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace SoDotCash.Models
 {
 
+    /// <summary>
+    /// A transaction represents a single atomic adjustment to the balance of an account - such as a withdrawal, deposit,
+    ///   credit or debit.
+    /// </summary>
     [Table("Transaction")]
     public class Transaction : EditableEntity
     {
@@ -16,25 +20,53 @@ namespace SoDotCash.Models
             CategoryName = "";
         }
 
+        /// <summary>
+        /// So.Cash unique database identifier (PK) for transaction. Internal use only.  Not displayed to end-user.
+        /// </summary>
         [Key]
         public int TransactionId { get; set; }
 
+        /// <summary>
+        /// Identifier of the account this transaction is associated with.
+        /// Do not use this field. Instead use the Account relationship link.
+        /// </summary>
         public int AccountId { get; set; }
 
+        /// <summary>
+        /// Transaction ID provided by financial institution (or entered by user if this is a manual entry account)
+        /// </summary>
         [StringLength(50)]
         public string FiTransactionId { get; set; }
 
+        /// <summary>
+        /// Description of this transaction.  User-entered for manual entry accounts. Downloaded from FI for auto-update accounts.
+        /// </summary>
         [StringLength(100)]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Optional category associated with this transaction. 
+        /// Can be used for sorting, statistics and higher level management operations in the future.
+        /// </summary>
         [StringLength(25)]
         public string CategoryName { get; set; }
 
+        /// <summary>
+        /// The date of the transaction. 
+        /// Time component is currently not provided by any known FIs and will be set to midnight.
+        /// </summary>
         [Column(TypeName="datetime")]
         public DateTime Date { get; set; }
 
+        /// <summary>
+        /// Amount of the transaction in integral units.
+        /// Use the currency to convert to localized units, or used LocalizedAmount accessor
+        /// </summary>
         public int Amount { get; set; }
 
+        /// <summary>
+        /// Relationship link to account holding this transaction
+        /// </summary>
         [InverseProperty("Transactions")]
         [ForeignKey("AccountId")]
         [Required]
@@ -48,6 +80,7 @@ namespace SoDotCash.Models
 
         /// <summary>
         /// Proxy property which converts the balance in cents into dollars
+        /// TODO: Handle different currency types
         /// </summary>
         public decimal LocalizedBalance => Balance/100m;
 
@@ -64,9 +97,7 @@ namespace SoDotCash.Models
         /// <summary>
         /// Property used in style templete to determine if the ammount is positive or negitive
         /// </summary>
-        public bool IsAmountPositive { get { return LocalizedAmount >= 0; } }
-
-
+        public bool IsAmountPositive => LocalizedAmount >= 0;
     }
 
 }
