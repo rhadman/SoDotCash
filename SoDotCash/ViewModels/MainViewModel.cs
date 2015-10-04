f802bfd95dcf4e240040c1b76d299b882fc882d9
Andrew Mann <andrew@divvycloud.com>
2015-10-03 21:55:25 -0500

    - Code cleanup

--- SoDotCash/ViewModels/MainViewModel.cs
+++ SoDotCash/ViewModels/MainViewModel.cs
@@ -1,34 +1,18 @@
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
-using OFX.Types;
-using SoDotCash.Services;
-using SoDotCash.Models;
 using SoDotCash.ViewModels.Navigation;
 
 namespace SoDotCash.ViewModels
 {
+
     /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
+    /// The main application window. This is a shell for views displayed within
     /// </summary>
     public class MainViewModel : ModernViewModelBase
     {
-        private readonly IModernNavigationService _modernNavigationService;
-
         #region [ Public Bound Properties ]
-
-        public ICommand ViewLoaded { get; }
 
         /// <summary>
         /// The ViewModel tied to the currently active view in the GUI
@@ -54,14 +38,17 @@ namespace SoDotCash.ViewModels
         /// </summary>
         public MainViewModel(IModernNavigationService navService)
         {
-            //assign the navigation service
-            _modernNavigationService = navService;
-
             ViewLoaded = new RelayCommand(Loaded);
-
-
         }
 
+        /// <summary>
+        /// Command handling the view loaded event
+        /// </summary>
+        public ICommand ViewLoaded { get; }
+
+        /// <summary>
+        /// Called when this view is loaded
+        /// </summary>
         private void Loaded()
         {
             NavigationCommands.GoToPage.Execute("/Views/Initializing.xaml",
