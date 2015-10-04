4b7056dbb614319933606997f178ed1f19fc2949
Andrew Mann <andrew@divvycloud.com>
2015-10-03 20:10:44 -0500

    - Rename SelectedValueToIsEnabled to IsNotNullConverter

--- SoDotCash/Converters/IsNotNullConverter.cs
+++ SoDotCash/Converters/IsNotNullConverter.cs
@@ -4,7 +4,12 @@ using System.Windows.Data;
 
 namespace SoDotCash.Converters
 {
-    public class SelectedValueToIsEnabled : IValueConverter
+    /// <summary>
+    /// Convert a value to a bool indicating whether the value is not null.
+    /// True - The value is not null
+    /// False - The value is null
+    /// </summary>
+    public class IsNotNullConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
